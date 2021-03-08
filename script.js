1fc0ee02d562a276a5aa03a6d279f3b8b3e2041f
Sarah Zhang <sarahz2476@hstat.org>
2021-03-08 21:02:28 +0000

    update repo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $.ajax({
-    url: 'https://api.publicapis.org/entries',
+    url: 'https://data.cityofnewyork.us/resource/uvw5-9znb.json',
     dataType: 'json',
     success: function(data) {
         console.log(data);
