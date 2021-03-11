eaa4368393fa0ce6400c3c31bc0db9ea82459d7a
Sarah Zhang <sarahz2476@hstat.org>
2021-03-11 02:05:54 +0000

    update repo

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ $.ajax({
     url: 'https://data.cityofnewyork.us/resource/uvw5-9znb.json',
     dataType: 'json',
     success: function(data) {
-        console.log(data);
+        var borough = prompt ("Input a borough (Make sure to Capitalize the first letter).");
+        document.write("<b> NYC Council Members that have district office in " + borough + ": </b><br>");
+       for(var i = 0; i < data.length; i++){
+           if (data[i].borough == borough){
+               console.log(data[i].name);
+               boroughString = "";
+               boroughString = data[i].name;
+               document.write(boroughString + "<br>");
+           };
+     
+       };
+        if (borough == "Brooklyn" || borough == "Queens"){
+               console.log(data[47].name);
+               BQString = "";
+               BQString = data[47].name;
+               document.write( BQString + "<br>");
+           };
+         if (borough == "Manhattan" || borough == "Bronx"){
+               console.log(data[26].name);
+               MBString = "";
+               MBString = data[26].name;
+               document.write( MBString + "<br>");
+           };
+     
+                      
     }
 });
