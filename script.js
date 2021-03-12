80f14198660f5dfaaff74d02e346881f2022604b
Sarah Zhang <sarahz2476@hstat.org>
2021-03-12 02:47:23 +0000

    update repo

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ $.ajax({
     url: 'https://data.cityofnewyork.us/resource/uvw5-9znb.json',
     dataType: 'json',
     success: function(data) {
+        // ask the user for a borough + user's answer is the value of the variable
+        var borough = prompt ("Input a New York borough (Make sure to Capitalize the first letters)."); 
+        // title
+        document.write("<b> NYC Council Members that have district office in " + borough + ": </b><br>");
         
-        var borough = prompt ("Input a New York borough (Make sure to Capitalize the first letters)."); // ask the user for a borough + user's answer is the value of the variable
-        document.write("<b> NYC Council Members that have district office in " + borough + ": </b><br>"); // title
       // this loop will go through the list and print out any council members' name with distric office in the borough the user asks for
        for(var i = 0; i < data.length; i++){
            if (data[i].borough == borough){
@@ -17,7 +19,7 @@ $.ajax({
        };
        
        // For these two conditions because two of the council member have district office in two different borough I had to make new condition for these two member.
-       // In the json for these two council members, the borough object is formatted "borough1 and borough 2".
+       // In the json for these two council members, the borough object is formatted "borough1 and borough2".
        // therefore I made two conditions specfically for two members.
         
         // so if they user input Brooklyn or Queens, this council member's name will print out
